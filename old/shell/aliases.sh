e61bb86bc056c51193e5318a3ba4f51cf1ed0b6b
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-11-01 12:21:12 +0100

    files

--- old/shell/aliases.sh
+++
@@ -1,4 +0,0 @@
-alias l='ls -lh --color=auto'
-alias ll='ls -lah --color=auto'
-# Add more aliases here
-
--- old/shell/shortcuts.sh
+++
@@ -1,2 +0,0 @@
-gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys control-center "['<Super>k']"
